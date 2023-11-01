1f792948fc7fbdc3257cbd98de1fc798905debdd
Keshav Kumar Jha <110764833+TheRealSaiTama@users.noreply.github.com>
2023-11-01 17:12:51 +0000

    Update main.py
    updated main.py for corresponding update in scoreboard.py for highscore option

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -37,12 +36,14 @@ while game_is_on:
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
